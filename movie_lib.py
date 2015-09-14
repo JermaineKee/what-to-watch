7f0c70327b79b4b66000889911c0d0c28ea4bf2e
Jermaine Kee <Jarvis@jermaines-mbp.dhe.duke.edu>
2015-09-13 23:51:22 -0400

    moving parts

--- movie_lib.py
+++ movie_lib.py
@@ -3,13 +3,45 @@ import csv
 all_movies = {}
 all_users = {}
 
+def users():
+    with open('ml-100k/u.user') as f:
+        reader = csv.DictReader(f, category = ['user_id'], delimeter = '|')
+        for row in reader:
+            User(int(row['user_id']))
+def movies():
+    with open('ml-100k/u.item', encoding = 'latin_1') as f:
+        reader = csv.DictReader(f, category = ['movie_id', 'movie_title', '', '', 'something_else' ],  delimeter = '|',)
+        for row in reader:
+            Movie(int(row['movie_id']), row['movie_title'])
+
+def ratings():
+    with open('ml_100k/u.data') as f:
+        reader = csv.DictReader(f, category = ['user_id', 'item_id', 'rating'], delimeter = '|' )
+        for row in reader:
+            Rating(int(row['user_id']), int(row['item_id']), int(row['rating']))
+
 
 class User:
-    def __init__(self, user_id):
+    "setting up 'User' class"
+    def __init__(self, user_id, rating):
         self.id = user_id
+        self.rating = {}
         all_users[self.id] = self
 
+
+    def add_user_rating(self, rating):
+        self.ratings[rating.item_id] = rating
+
+    def get_ratings(self):
+        return sorted(self.ratings.values(), reverse=True)
+
+    def get_top_rated(self,num):
+        return sorted([x for x in all_movies.values()
+                        if x.id not in self.ratings],
+                    key = lambda m: m.avg_ratings(), reverse = True) [:num]
+
 class Movie:
+    "setting up 'Movie' class"
     def __init__(self, movie_id, title):
         self.id = movie_id
         self.title = title
@@ -20,7 +52,10 @@ class Movie:
         self.ratings[rating.user_id] = rating
 
     def get_ratings(self):
-        return self.ratings.values()
+        return sorted(self.ratings.values(), reverse=True)
+
+    def ave_ratings(self):
+        return sum(get_ratings(self))/len(get_ratings(self))
 
 
     def __str__(self):
@@ -28,18 +63,39 @@ class Movie:
 
     def __repr__(self):
         return self.__str__()
-   
+
 
 class Rating:
+    "setting up 'Rating' class"
     def __init__(self, user_id, movie_id, stars):
         self.user_id = user_id
         self.movie_id = movie_id
         self.stars = stars
 
         all_movies[self.movie_id].add_rating(self)
+        all_users[self.user_id].add_user_rating(self)
+
 
     def __str__(self):
         return 'Rating(user_id = {}, movie_id = {}, stars ={})'.format(self.id)
+
+
+def euclidean_distance(v, w):
+    """Given two lists, give the Euclidean distance between them on a scale
+    of 0 to 1. 1 means the two lists are identical.
+    """
+
+    # Guard against empty lists.
+    if len(v) is 0:
+        return 0
+
+    # Note that this is the same as vector subtraction.
+    differences = [v[idx] - w[idx] for idx in range(len(v))]
+    squares = [diff ** 2 for diff in differences]
+    sum_of_squares = sum(squares)
+
+    return 1 / (1 + math.sqrt(sum_of_squares))
+
 
 
 #users =
--- test_movie_lib.py
+++ test_movie_lib.py
@@ -1,22 +1,27 @@
 from movie_lib import *
 
-ebert = user(5)
-siskel = user(12)
+user1 = User(5)
+user2 = User(12)
+user3 = User(345)
+
 movie1 = Movie(3, 'Toy Story')
 movie2 = Movie(9, 'Pretty Woman')
 
-rating1 = Rating(siskel.id, movie1.id, 4)
-rating2 = Rating(ebert.id, movie2.id, 1)
-rating3 = Rating(siskel.id, movie2.id, 3)
-rating4 = Rating(ebert.id, movie1.id, 1)
+rating1 = Rating(200, 15, 4)
+rating2 = Rating(user1.id, movie2.id, 1)
+rating3 = Rating(user2.id, movie2.id, 3)
+rating4 = Rating(user3.id, movie1.id, 1)
 
-def tes_user_creation():
-    user1 = User(5)
-    user2 = User(12)
+print(all_movies)
+print(all_users)
+
+def test_user_creation():
+
     assert user1.id == 5
     assert user2.id == 12
+    assert user3.id == 222
 
-   
+
 def tes_movie_creation():
     movie1 = Movie(3, 'Toy Story')
     movie2 = Movie(9, 'Pretty Woman')
@@ -26,8 +31,8 @@ def tes_movie_creation():
     assert movie2.title == 'Pretty Woman'
 
 def test_rating_creation():
-    assert rating1.user_id == rating3.user_id == siskel.id
-    assert rating2.user_id == rating4.user_id == ebert.id
+    assert rating1.user_id == rating3.user_id == user1.id
+    assert rating2.user_id == rating4.user_id == user2.id
     assert ratings1.movie_id == rating4.movie_id == movie1.id
     assert ratings2.movie_id == rating3.movie_id == movie2.id
 
@@ -37,3 +42,6 @@ def test_find_ratings_for_movie():
     # or
     # returns a list of all ratings for that movie
     toy_story_ratings = all_movies[movie1.id].get_ratings()
+    print(len(all_users))
+    print(all_users)
+    assert len(all_users) == 3
